c6d801cf29f752e514a31f52b4759e5c7ca1fe19
2lavea02 <2lavea02@solent.ac.uk>
2018-03-12 12:29:50 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2lavea02/asyncactivity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a2lavea02.asyncactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+abstract public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
